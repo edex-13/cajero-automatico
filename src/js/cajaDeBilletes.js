f8b681de9d8a404d08f1dfa9eb1bea30c994eabb
Edex13 <ederson12f@gmail.com>
2021-03-28 17:49:08 -0500

    soluciono bug en las rutas de las imagenes

--- src/js/cajaDeBilletes.js
+++ src/js/cajaDeBilletes.js
@@ -7,11 +7,11 @@ class Billete {
    }
 }
 let cajaDeBilletes = [];
-cajaDeBilletes.push(new Billete(500, 100, '/Img/500.png'));
-cajaDeBilletes.push(new Billete(200, 100, '/Img/200.png'));
-cajaDeBilletes.push(new Billete(100, 100, '/Img/100.png'));
-cajaDeBilletes.push(new Billete(50, 100, '/Img/50.png'));
-cajaDeBilletes.push(new Billete(20, 100, '/Img/20.png'));
-cajaDeBilletes.push(new Billete(10, 100, '/Img/10.png'));
+cajaDeBilletes.push(new Billete(500, 100, 'Img/500.png'));
+cajaDeBilletes.push(new Billete(200, 100, 'Img/200.png'));
+cajaDeBilletes.push(new Billete(100, 100, 'Img/100.png'));
+cajaDeBilletes.push(new Billete(50, 100, 'Img/50.png'));
+cajaDeBilletes.push(new Billete(20, 100, 'Img/20.png'));
+cajaDeBilletes.push(new Billete(10, 100, 'Img/10.png'));
 
 export default cajaDeBilletes;
